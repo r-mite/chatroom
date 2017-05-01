ffc48bf9d5f4f5a5da8d307d45a8764f9074e351
永野峻輔 <peace.infi0407@gmail.com>
2017-05-01 18:28:09 +0900

    ゴミの削除

--- app/app.js
+++ app/app.js
@@ -14,7 +14,6 @@ app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -23,8 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(favicon(path.join(__dirname, '../public/','images/favicon.ico')));
 app.use('/', index);
-
-//app.use(express.favicon(path.join(__dirname, 'public/images/favicon.ico')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
